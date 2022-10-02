41bf2e691de694dc2599585f3e7abe31dc4a43a0
Tyler Twisle <102629991+TwisleOfficial@users.noreply.github.com>
2022-10-02 16:12:29 -0400

    Added more options for it to generate
    Also added lines in between each choice to make it easier to read.

--- Generator.py
+++ Generator.py
@@ -1,23 +1,27 @@
 import random
 
-location = ['shopping mall', 'forest', 'cave', 'cloud', 'school', 'wild west']
-genre = ['rogue', 'battle royal', 'FPS', 'MMO', 'RPG', 'survival']
-goal = ['kill everything', 'escape', 'run', 'hide', 'farm']
-rule = ['no jumping', 'no sound', 'one direction', 'limited lives']
-wildcard = ['dark colors', 'bright colors', 'low-poly', 'pixel art', 'platformer']
+location = ['Shopping mall', 'Forest', 'Cave', 'Clouds', 'School', 'Wild west', 'Farm', 'A house', 'Underground', 'In A Ant Colony', 'In A Big City']
+genre = ['Rogue', 'Battle Royal', 'FPS', 'MMO', 'RPG', 'Survival', 'Puzzle', 'Simulation', 'City Builder', 'Strategy', "Sports", 'Sandbox', 'Platformer']
+goal = ['kill everything', 'escape', 'run', 'hide', 'Be the best', 'Be the worst', 'Have the best outfit', 'Destory everything', 'Build something']
+rule = ['No jumping', 'No sound', 'One direction', 'Limited lives', 'No light', 'One bullet', 'Time Limit']
+wildcard = ['Dark colors', 'Bright colors', 'Low-poly', 'Pixel art', 'Platformer', '2D', '3D', 'Steampunk', 'World is ending']
+
+lines = "--------------------"
 
 while True:
     answer = input("Generate? (y/n)")
     if answer == "y":
-        print("--------------------")
-        print("                    ")
+        print(lines)
         print("location - "+random.choice(location))
+        print(lines)
         print("genre - "+random.choice(genre))
+        print(lines)
         print("goal - "+random.choice(goal))
+        print(lines)
         print("rule - "+random.choice(rule))
+        print(lines)
         print("wildcard - "+random.choice(wildcard))
-        print("                    ")
-        print("--------------------")
+        print(lines)
 
         continue
 
